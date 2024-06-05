8a8782f82389161c1ad6e5084b9b44386aef1aa2
scottg <scottghome@scottgs-MBP.lan>
2024-06-05 08:32:25 -0500

    update

--- custom_vpc.tf
+++ custom_vpc.tf
@@ -64,7 +64,7 @@ resource "aws_route_table_association" "Private-RT-Association-2A" {
   route_table_id = aws_route_table.Private-RT.id
 }
 
-# associate private subnet 1B with private route table
+# associate private subnet 2B with private route table
 resource "aws_route_table_association" "Private-RT-Association-2B" {
   subnet_id      = aws_subnet.Private-2B.id
   route_table_id = aws_route_table.Private-RT.id
@@ -79,39 +79,132 @@ resource "aws_internet_gateway" "MyIGW" {
   }
 }
 
-# add IGW route to main route table
+# add IGW route to main route table for MyVPC
 resource "aws_route" "IGW-Route" {
-  route_table_id         = aws_vpc.MyVPC.main_route_table_id
+  route_table_id         = aws_vpc.MyVPC.main_route_table_id # IGW in main RT
   gateway_id             = aws_internet_gateway.MyIGW.id
   depends_on             = [aws_internet_gateway.MyIGW]
   destination_cidr_block = "0.0.0.0/0"
 }
 
-# create EIP for NAT gateway in Public 2a & 2b
+# create SG for public subnet
+resource "aws_security_group" "Public-SG" {
+  name   = "Public-SG"
+  vpc_id = aws_vpc.MyVPC.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# create EIP for NAT gateway
 resource "aws_eip" "nat_gateway_eip" {
   domain = "vpc"
   tags = {
     Name        = "MyEIP"
-    Description = "EIP from Advanced Networking Hands on Lab"
+    Description = "EIP for NAT Gateway"
   }
 }
 
+output "eip_address" {
+  description = "EIP public address"
+  value       = aws_eip.nat_gateway_eip.public_ip
+}
 
-# create NAT gateway in Public subnet 2a & 2b
-resource "aws_nat_gateway" "MyNAT" {
+# create NAT gateway in Private subnet 2a
+resource "aws_nat_gateway" "MyNATGW" {
   allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id     = aws_subnet.Public-2A.id
+  subnet_id     = aws_subnet.Private-2A.id
   tags = {
-    Name        = "MyNAT"
-    Description = "NAT Gateway from Advanced Networking Hands on Lab"
+    Name        = "MyNATGW"
+    Description = "NAT Gateway in Private subnet"
   }
   depends_on = [aws_eip.nat_gateway_eip]
 }
 
-# add route to NAT gateway
-resource "aws_route" "NAT-Route" {
-  route_table_id         = aws_route_table.Private-RT.id
-  nat_gateway_id         = aws_nat_gateway.MyNAT.id
-  destination_cidr_block = "0.0.0.0/0"
-  depends_on             = [aws_nat_gateway.MyNAT]
+# create a security group for the NAT gateway
+resource "aws_security_group" "NAT-SG" {
+  name   = "NAT-SG"
+  vpc_id = aws_vpc.MyVPC.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# allow inbound traffic on the NAT SG
+resource "aws_security_group_rule" "NAT-SG-Inbound" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.NAT-SG.id
+  cidr_blocks       = ["0.0.0.0/0"]
+
+}
+
+# allow outbound traffic on the NAT SG
+resource "aws_security_group_rule" "NAT-SG-Outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.NAT-SG.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# create ec2 instance in routable public subnet 2a
+resource "aws_instance" "MyInstance1" {
+  ami             = "ami-0c9921088121ad00b"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.Public-2A.id
+  security_groups = [aws_security_group.Public-SG.id]
+
+  user_data = <<-EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello, World!</h1>" > /var/www/html/index.html
+EOF
+
+  tags = {
+    Name = "ec2_1"
+  }
+}
+
+# create ec2 instance in public subnet 2b
+resource "aws_instance" "MyInstance2" {
+  ami             = "ami-0c9921088121ad00b"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.Public-2B.id
+  security_groups = [aws_security_group.Public-SG.id]
+
+  user_data = <<-EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello, World!</h1>" > /var/www/html/index.html
+EOF
+
+  tags = {
+    Name = "ec2_2"
+  }
 }
