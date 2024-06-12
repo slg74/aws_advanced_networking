d8211edd2ad4ff0d0c274b17b1f4f7bdb8f652ea
scottg <scottghome@scottgs-MBP.lan>
2024-06-11 20:58:59 -0500

    ansible instances in public subnet in custom vpc

--- custom_vpc.tf
+++ custom_vpc.tf
@@ -22,40 +22,12 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-# Create a private subnet
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "private-subnet"
-  }
-}
-
 # Create an internet gateway
 resource "aws_internet_gateway" "example_igw" {
   vpc_id = aws_vpc.example_vpc.id
 
   tags = {
     Name = "example-igw"
-  }
-}
-
-# Create a NAT gateway
-resource "aws_nat_gateway" "example_nat_gw" {
-  allocation_id = aws_eip.nat_gw_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
-
-  tags = {
-    Name = "example-nat-gw"
-  }
-}
-
-# Create an Elastic IP for the NAT gateway
-resource "aws_eip" "nat_gw_eip" {
-  tags = {
-    Name = "nat-gw-eip"
   }
 }
 
@@ -79,30 +51,10 @@ resource "aws_route_table_association" "public_rt_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Create a route table for the private subnet
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.example_vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.example_nat_gw.id
-  }
-
-  tags = {
-    Name = "private-rt"
-  }
-}
-
-# Associate the private subnet with the private route table
-resource "aws_route_table_association" "private_rt_assoc" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
 # Create a security group for the EC2 instances
 resource "aws_security_group" "example_sg" {
   name        = "example-sg"
-  description = "Allow SSH inbound traffic"
+  description = "Allow SSH"
   vpc_id      = aws_vpc.example_vpc.id
 
   ingress {
@@ -124,12 +76,12 @@ resource "aws_security_group" "example_sg" {
   }
 }
 
-# Create 4 EC2 instances with Elastic IPs in the private subnet, to later be managed with Ansible
+# Create 5 EC2 instances in the public subnet
 resource "aws_instance" "example" {
-  count                  = 3
+  count                  = 5
   ami                    = "ami-0c9921088121ad00b"
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.private_subnet.id
+  subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.example_sg.id]
   key_name               = "ansible_tf_keypair"
 
@@ -138,21 +90,4 @@ resource "aws_instance" "example" {
   tags = {
     Name = "example-instance-${count.index}"
   }
-}
-
-# Create Elastic IPs for the EC2 instances
-resource "aws_eip" "example_eip_instances" {
-  count = 3
-
-  instance                  = aws_instance.example[count.index].id
-  associate_with_private_ip = aws_instance.example[count.index].private_ip
-
-  tags = {
-    Name = "example-eip-instance-${count.index}"
-  }
-}
-
-output "instance_elastic_ips" {
-  description = "Elastic IP addresses of the EC2 instances"
-  value       = aws_eip.example_eip_instances[*].public_ip
 }
